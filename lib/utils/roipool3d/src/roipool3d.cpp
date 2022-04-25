b9b8c0669d6b3f0e8070af55d0746e13b8ad7876
YangXiuyu <gzzyyxy@gmail.com>
2022-04-25 14:24:14 +0800

    Update roipool3d.cpp

--- lib/utils/roipool3d/src/roipool3d.cpp
+++ lib/utils/roipool3d/src/roipool3d.cpp
@@ -2,8 +2,8 @@
 #include <torch/extension.h>
 
 
-#define CHECK_CUDA(x) AT_CHECK(x.type().is_cuda(), #x, " must be a CUDAtensor ")
-#define CHECK_CONTIGUOUS(x) AT_CHECK(x.is_contiguous(), #x, " must be contiguous ")
+#define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x, " must be a CUDAtensor ")
+#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x, " must be contiguous ")
 #define CHECK_INPUT(x) CHECK_CUDA(x);CHECK_CONTIGUOUS(x)
 
 void roipool3dLauncher_slow(int batch_size, int pts_num, int boxes_num, int feature_in_len, int sampled_pts_num, 
